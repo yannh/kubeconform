a23275d5ca0531b7af5489ef6fd3dc321dd322c5
Yann Hamon <yannh@users.noreply.github.com>
2025-05-12 10:21:02 +0200

    Invalid JSON should not be considered an error - see https://github.com/yannh/kubeconform/issues/67 (#327)

--- pkg/loader/loaders.go
+++ pkg/loader/loaders.go
@@ -10,3 +10,13 @@ func NewNotFoundError(err error) *NotFoundError {
 }
 func (e *NotFoundError) Error() string   { return e.err.Error() }
 func (e *NotFoundError) Retryable() bool { return false }
+
+type NonJSONResponseError struct {
+	err error
+}
+
+func NewNonJSONResponseError(err error) *NotFoundError {
+	return &NotFoundError{err}
+}
+func (e *NonJSONResponseError) Error() string   { return e.err.Error() }
+func (e *NonJSONResponseError) Retryable() bool { return false }
